2c0371e1ebe242072b2d3993a9cb8cd3cf14775e
Mark De May <markdemay@TirNaNog.PK5001Z>
2017-05-24 10:43:53 -0600

    Set up main controller

---
+++ public/js/controllers/main.js
@@ -0,0 +1,23 @@
+(function() {
+
+  angular
+    .module('artFromLightApp')
+    .controller('MainController', MainController);
+
+  MainController.$inject = ['$http'];
+
+  function MainController($http) {
+
+    var URL;
+    if (window.location.href === 'http://localhost:3001/') {
+      URL = 'http://localhost:3000/';
+    } else {
+      URL = 'https://art_from_light_app.com/';
+    }
+
+    const vm = this;
+
+    vm.here = "this is the start"
+
+  }
+})();
