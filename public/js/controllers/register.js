bc45a60775612bfafec743b463e497669d75c592
Mark De May <markdemay@TirNaNog.PK5001Z>
2017-05-27 15:21:42 -0600

    Register new user

--- public/js/controllers/register.js
+++ public/js/controllers/register.js
@@ -16,6 +16,39 @@
 
       const vm = this;
 
-      
+      vm.user = {};
+      vm.register = register;
+
+      function register(main) {
+
+        // User name and password are required
+        if (vm.user.username === undefined ||
+            vm.user.username === "" ||
+            vm.user.password === undefined ||
+            vm.user.password === "") {
+          return;
+        }
+
+        $http({
+          method: 'POST',
+          url: URL + 'users',
+          data: {
+            user: {
+              name: vm.user.name,
+              username: vm.user.username,
+              address: vm.user.address,
+              password: vm.user.password
+            }
+          }
+        }).then( function( response) {
+          main.selectedUserId = response.data.id;
+          main.userID = response.data.id;
+          main.userName = response.data.name;
+          main.curPage = main.userPage;
+        }, function( error) {
+          console.log("register.register ", error);
+        })
+      }
+
     }
 })();
